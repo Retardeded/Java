4a0ca41064f731fff97508df2609dd0bb7622238
NeedThisAccount <36775976+Retardeded@users.noreply.github.com>
2019-11-05 08:26:41 +0100

    Delete ParagraohWithList.java

--- ParagraohWithList.java
+++
@@ -1,9 +0,0 @@
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
-
-public class ParagraohWithList extends Paragraph{
-
-    Set<Unorderedlist> names = new Set<Unorderedlist>();
-
-}
