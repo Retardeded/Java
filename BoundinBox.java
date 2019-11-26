002a02b2c114b1ad5eea2dd8e74f516f150a628d
NeedThisAccount <36775976+Retardeded@users.noreply.github.com>
2019-11-26 08:57:39 +0100

    Create BoundinBox.java

---
+++ BoundinBox.java
@@ -0,0 +1,20 @@
+public class BoundingBox {
+  double xMin;
+  double xMax;
+  double yMin;
+  double yMax;
+
+  @Override
+  public String toString() {
+    return "BoundingBox{"
+        + "xMin="
+        + xMin
+        + ", xMax="
+        + xMax
+        + ", yMin="
+        + yMin
+        + ", yMax="
+        + yMax
+        + '}';
+  }
+}
